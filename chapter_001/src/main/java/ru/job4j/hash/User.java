de2e259629f7eb706dc0309119751ff2a569c0a5
reybos <andreybosiy@yandex.ru>
2020-11-21 16:35:59 +0300

    2. Без переопределения equals и hashCode [#404141]

--- chapter_001/src/main/java/ru/job4j/hash/User.java
+++ chapter_001/src/main/java/ru/job4j/hash/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.hash;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,32 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> storage = new HashMap<>();
+        User user1 = new User(
+                "Andrew",
+                0,
+                new GregorianCalendar(1991, Calendar.APRIL, 14)
+        );
+        User user2 = new User(
+                "Andrew",
+                0,
+                new GregorianCalendar(1991, Calendar.APRIL, 14)
+        );
+        storage.put(user1, user1);
+        storage.put(user2, user2);
+        for (User key : storage.keySet()) {
+            System.out.println(key + " - " + storage.get(key));
+        }
     }
 }
