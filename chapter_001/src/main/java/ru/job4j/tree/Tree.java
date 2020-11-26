10d2a6a2666c6906e673679909fc3a5f0460b376
reybos <andreybosiy@yandex.ru>
2020-11-26 17:18:23 +0300

    2. Добавить метод boolean isBinary() [#404127]

--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -1,27 +1,13 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.BiPredicate;
 
-class Tree<E> implements SimpleTree<E> {
+public class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
 
     Tree(final E root) {
         this.root = new Node<>(root);
-    }
-
-    public boolean isBinary() {
-        boolean rsl = true;
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.children.size() > 2) {
-                rsl = false;
-                break;
-            }
-            data.addAll(el.children);
-        }
-        return rsl;
     }
 
     @Override
@@ -33,14 +19,24 @@ class Tree<E> implements SimpleTree<E> {
         return parentNode.get().children.add(new Node<>(child));
     }
 
+    public boolean isBinary() {
+        BiPredicate<Node<E>, E> func = (node, val) -> node.children.size() > 2;
+        return findElemByPredicate(func, null).isEmpty();
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
+        BiPredicate<Node<E>, E> func = (node, val) -> node.value.equals(val);
+        return findElemByPredicate(func, value);
+    }
+
+    private Optional<Node<E>> findElemByPredicate(BiPredicate<Node<E>, E> func, E value) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (func.test(el, value)) {
                 rsl = Optional.of(el);
                 break;
             }
