580f608178e66332ca7ec594c415172b2b0d2e5f
reybos <andreybosiy@yandex.ru>
2020-11-29 18:30:16 +0300

    чтение файла построчно

--- chapter_002/src/main/java/ru/job4j/io/ReadFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ReadFile.java
@@ -1,9 +1,14 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadFile {
     public static void main(String[] args) {
+        // чтение побайтно
         try (FileInputStream in = new FileInputStream("input.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
@@ -12,6 +17,16 @@ public class ReadFile {
             }
             System.out.println(text);
             String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //чтение по строчно
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            List<String> lines = new ArrayList<>();
+            in.lines().forEach(lines::add);
             for (String line : lines) {
                 System.out.println(line);
             }
