2a81dc570db6d93855b73205ca434cfb0c348787
reybos <andreybosiy@yandex.ru>
2020-11-30 14:24:09 +0300

    1. Читаем файл конфигурации [#404163]

---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.dialect"),
+                is("org.hibernate.dialect.PostgreSQLDialect")
+        );
+        assertThat(config.size(), is(5));
+    }
+}
