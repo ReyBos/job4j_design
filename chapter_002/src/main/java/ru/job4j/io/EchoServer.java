ad2ac51359304fa64b76a465f9bbf0e58179ab68
reybos <andreybosiy@yandex.ru>
2020-12-04 23:41:13 +0300

    1. Бот [#404180]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,12 +1,15 @@
-package ru.job4j.socket;
+package ru.job4j.io;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
     //curl -i http://localhost:9000/?msg=Hello
     //Параметр -i указывает curl вывести всю информацию принятую от сервера.
+    @SuppressWarnings("checkstyle:InnerAssignment")
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (true) {
@@ -17,16 +20,32 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    String[] line;
+                    String str, answer = "";
                     boolean needStop = false;
                     for (int i = 0; !(str = in.readLine()).isEmpty(); i++) {
-                        if (i == 0 && (line = str.split("=")).length == 2) {
-                            needStop = line[1].startsWith("Bye");
+                        if (i == 0) {
+                            Pattern pattern = Pattern.compile("[a-zA-Z]+=[a-zA-Z]+");
+                            Matcher matcher = pattern.matcher(str);
+                            if (matcher.find()) {
+                                String[] arg = matcher.group().split("=");
+                                switch (arg[1]) {
+                                    case "Exit":
+                                        needStop = true;
+                                        break;
+                                    case "Hello":
+                                        answer = "Hello";
+                                        break;
+                                    default:
+                                        answer = "What";
+                                }
+                            }
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    if (!answer.isEmpty()) {
+                        out.write(answer.getBytes());
+                    }
                     if (needStop) {
                         break;
                     }
