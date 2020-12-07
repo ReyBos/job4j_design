634b72cd5a427b82eb70a90ae54e6da3944cb45c
reybos <andreybosiy@yandex.ru>
2020-12-07 22:29:42 +0300

    1. Log4j. Логирование системы. [#404149]

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,4 +1,4 @@
-package ru.job4j.log;
+package ru.job4j.io;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +7,11 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
         String name = "Petr Arsentev";
         int age = 33;
         LOG.debug("User info name : {}, age : {}", name, age);
