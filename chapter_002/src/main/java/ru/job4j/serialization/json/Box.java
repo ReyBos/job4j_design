fcee06d1db81873a3cdcf66135f1d625f11e6d20
reybos <andreybosiy@yandex.ru>
2020-12-10 21:54:13 +0300

    5. Преобразование JSON в POJO. JsonObject [#404173]

--- chapter_002/src/main/java/ru/job4j/serialization/json/Box.java
+++ chapter_002/src/main/java/ru/job4j/serialization/json/Box.java
@@ -2,8 +2,12 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Box {
     private boolean isEmpty;
@@ -20,6 +24,18 @@ public class Box {
         this.content = content;
     }
 
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     @Override
     public String toString() {
         return "Box{"
@@ -32,6 +48,11 @@ public class Box {
     }
 
     public static void main(String[] args) {
+        JSONObject jsonMaterial = new JSONObject("{\"name\": \"cardboard\"}");
+        List<String> list = new ArrayList<>();
+        list.add("Pens");
+        list.add("Pencils");
+        JSONArray jsonContent = new JSONArray(list);
         final Box box = new Box(
                 false,
                 4,
@@ -39,10 +60,28 @@ public class Box {
                 new Material("cardboard"),
                 "Pens", "Pencils"
         );
-        final Gson gson = new GsonBuilder().create();
-        final String boxJson = gson.toJson(box);
-        System.out.println(boxJson);
-        final Box boxMod = gson.fromJson(boxJson, Box.class);
-        System.out.println(boxMod);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("isEmpty", box.isEmpty());
+        jsonObject.put("capacity", box.getCapacity());
+        jsonObject.put("color", box.getColor());
+        jsonObject.put("material", jsonMaterial);
+        jsonObject.put("content", jsonContent);
+        System.out.println(jsonObject.toString());
+        System.out.println(new JSONObject(box).toString());
     }
+
+//    public static void main(String[] args) {
+//        final Box box = new Box(
+//                false,
+//                4,
+//                "Red",
+//                new Material("cardboard"),
+//                "Pens", "Pencils"
+//        );
+//        final Gson gson = new GsonBuilder().create();
+//        final String boxJson = gson.toJson(box);
+//        System.out.println(boxJson);
+//        final Box boxMod = gson.fromJson(boxJson, Box.class);
+//        System.out.println(boxMod);
+//    }
 }
